3edaa31e27dba733ca55e98ea4c2f4e34e48ebfd
Ubuntu <ubuntu@ip-172-31-81-161.ec2.internal>
2019-09-18 23:13:50 +0000

    First hello

---
+++ game.c
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 me All rights reserved.
+//
+// Created by: Matsuru Hoshi
+// Created on: Sept 2019
+// This program is the "Hello, World!" program for the GameBoy
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main() {
+    printf("Hello, World!");
+
+    return 0;
+}
